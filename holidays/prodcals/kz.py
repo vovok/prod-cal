d9f4bfeb6834e37fa642303b0b9d72a754030373
Arkady Aristov <arkadyaristov@gmail.com>
2016-04-05 12:20:49 -0400

    Загрузил файл календаря праздников Казахстана

---
+++ holidays/prodcals/kz.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Production calendar for Kazakhstan"""
+
+from prod_dict import ProdDict
+
+NON_WORK_DAY_DICT = ProdDict({
+    2016: {1: [1, 4],
+           2: [],
+           3: [8, 21, 22, 23],
+           4: [],
+           5: [2, 9, 10],
+           6: [],
+           7: [6],
+           8: [30],
+           9: [13],
+           10: [],
+           11: [],
+           12: [1, 16, 19]}
+})
+
+
+WORK_DAY_DICT = ProdDict({
+    2016: {1: [],
+           2: [],
+           3: [],
+           4: [],
+           5: [],
+           6: [],
+           7: [],
+           8: [],
+           9: [],
+           10: [],
+           11: [],
+           12: []}
+})
